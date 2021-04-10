840e6c30452517db62c248188f11dd2fa2ae5fc0
aishwaryashelar3097 <imaishwaryashelar@gmail.com>
2021-04-10 20:00:07 +0530

    added CabInvoiceGenerator1

---
+++ src/main/java/CabInvoiceGenerator1.java
@@ -0,0 +1,51 @@
+
+public class CabInvoiceGenerator1 {public class CabInviceGenerator {
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		new_InvoiceGenerator()
+		Assert.assertEquals (expected:25, fare)
+	}
+	public class InvoiceGenerator {
+		public double calculateFare(double distance, int time) {
+			private static final int COST_PER_TIME = 1;
+			double fare = InvoiceGenerator
+		}
+	}
+@Test
+public void givenLessDistanceOrTime_ShouldReturnMinFare() {
+	ShouldReturnMinFare() {
+		double distance = 0.1;
+		double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time
+				if (totalFare < MINIMUM_FARE)
+					Assert.assertEquals (expected:5, fare);
+	}
+}
+@Test
+public void givenMultipleRides_ShouldReturnTotalFare() {
+	public Ride (double distance:2.0,5);
+}
+Ride [] rides = new Ride (distance:2.0, time :5),
+new Ride (distance:0.1,time:1)
+invoice Generator.calculateFare(rides);
+}
+double fare = invoiceGenerator.calculateFare(rides);
+public Ride (double distance, int time) {
+	this.distance=distance;
+	this.time = time;
+	public final int
+	this.calculateFare(ride.distance, ride.time);
+	totalFare=this.calculator
+}
+@Before
+public void setup() throws Exception {
+	public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+		new InvoiceSummary(2,30.0);
+		Assert.assertEquals(expectedInvoiceSummary)
+		this.numOfRides=numOfRides;
+		this.totalFare=totalFare;
+		this.averageFare=this.givenDistanceAndTime_ShouldReturnTotalFare();
+		return new InvoiceSummary(rides.length)
+	}
+}
+
+
+}
